140b47487d6935d2fcf4068bc5db621f6d8c5818
Nael Shiab <nael.shiab@gmail.com>
2022-08-12 13:33:23 -0400

    New export

--- components/Keys.tsx
+++ components/Keys.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 export default function Keys({ id, method, generateArgId, updateArgs, d, i, simpleData }) {
     return <select id={generateArgId(id, i, method)} onChange={() => updateArgs()} defaultValue={d.defaultValue}>
         {simpleData ? [undefined, ...simpleData.getKeys()].map((opt, index) => <option key={`${method}-option-${index}`}>{opt}</option>
