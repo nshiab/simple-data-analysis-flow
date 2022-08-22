473d7d685eac6067f5d2b935ba33ae65e7de3cb8
Nael Shiab <nael.shiab@gmail.com>
2022-08-22 14:03:19 -0400

    Version 0.8.0

--- docs/_next/static/GAg2uP6-0bMPDgGPzSi6E/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/0a2f8ba0-c0bea8b262396d37.js","static/chunks/801-fa7e44d3fc7b6b5a.js","static/chunks/pages/index-077fc110f6a29594.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
