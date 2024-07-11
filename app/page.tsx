f59a1ed2bdae8e3c4de512c44f2b7e54f2a7808c
Nael Shiab <nael.shiab@gmail.com>
2024-07-11 09:44:18 -0400

    Github log

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,27 @@
+import GithubLogo from "@/components/partials/GithubLogo";
 import SimpleFlow from "@/components/SimpleFlow";
 
 export default function Home() {
   return (
     <main>
-      <div className="container pt-8 fixed z-10 bg-white pb-4">
-        <h1 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-          Simple Data Analysis Flow
-        </h1>
+      <div className="container pt-6 fixed z-10 bg-white pb-4">
+        <div className="flex items-center justify-between">
+          <h1 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+            Simple Data Analysis Flow
+          </h1>
+          <GithubLogo />
+        </div>
         <p className="leading-7 [&:not(:first-child)]:mt-2">
           This is a node-based editor for{" "}
           <a
             className="font-medium underline underline-offset-2"
+            target="_blank"
             href="https://github.com/nshiab/simple-data-analysis.js"
           >
             simple-data-analysis
           </a>{" "}
           that runs in your browser. To add nodes, right-click on the panel
-          below. If you encounter a bug, create an issue on{" "}
-          <a
-            className="font-medium underline underline-offset-2"
-            href="https://github.com/nshiab/simple-data-analysis-flow"
-          >
-            GitHub
-          </a>
-          .
+          below.
         </p>
       </div>
       <SimpleFlow />
