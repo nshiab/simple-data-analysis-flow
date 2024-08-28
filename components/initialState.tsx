bfd38249a66d5edd383e89cd7ad3432e9c0efda0
Nael Shiab <nael.shiab@gmail.com>
2024-08-28 14:11:42 -0400

    Changes saved to localStorage every 1s

--- components/initialState.tsx
+++ components/initialState.tsx
@@ -1,34 +1,22 @@
-import { getId } from "journalism"
+import getNewState from "./helpers/getNewState"
 import { Edge, Node } from "@xyflow/react"
 
-const dbId = `node_${getId()}`
-const tableId = `node_${getId()}`
+let initialNodes: Node[] = []
+let initialEdges: Edge[] = []
 
-const initialNodes: Node[] = [
-  {
-    id: dbId,
-    type: "SDB",
-    position: { x: 0, y: 0 },
-    data: { instance: null },
-    origin: [0.5, 0],
-  },
-  {
-    id: tableId,
-    type: "ST",
-    position: { x: -50, y: 150 },
-    data: { instance: null, name: tableId },
-    origin: [0.5, 0],
-  },
-]
+let recoveredFlow = localStorage.getItem("sda-flow")
 
-const edgeId = `edge_${getId()}`
-
-const initialEdges: Edge[] = [
-  {
-    id: edgeId,
-    source: dbId,
-    target: tableId,
-  },
-]
+if (recoveredFlow) {
+  const flow = JSON.parse(recoveredFlow)
+  initialNodes = flow.nodes.map((d: Node) => ({
+    ...d,
+    data: { ...d.data, imported: true },
+  }))
+  initialEdges = flow.edges
+} else {
+  const newState = getNewState()
+  initialNodes = newState.nodes
+  initialEdges = newState.edges
+}
 
 export { initialNodes, initialEdges }
